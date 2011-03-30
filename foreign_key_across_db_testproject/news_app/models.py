6d8806d739bfde2f7d5a124bba1c193651c43ab8
Eliot <eliot@tapir>
2011-03-30 15:07:09 -0700

    Hack to make fruit_name() work

--- foreign_key_across_db_testproject/news_app/models.py
+++ foreign_key_across_db_testproject/news_app/models.py
@@ -8,6 +8,18 @@ class Article(models.Model):
     fruit = ForeignKeyAcrossDb(Fruit)
     intro = models.TextField()
 
+    # TODO: shouldn't need fruit_obj if ForeignKeyAcrossDb field worked properly
+    @property
+    def fruit_obj(self):
+        if not hasattr(self, '_fruit_obj'):
+            # TODO: why is it sometimes an int and sometimes a Fruit object?
+            if isinstance(self.fruit, int) or isinstance(self.fruit, long):
+                print 'self.fruit IS a number'
+                self._fruit_obj = Fruit.objects.get(pk=self.fruit)
+            else:
+                print 'self.fruit IS NOT a number'
+                self._fruit_obj = self.fruit
+        return self._fruit_obj
+
     def fruit_name(self):
-        print self.fruit
-        return self.fruit.name
+        return self.fruit_obj.name
