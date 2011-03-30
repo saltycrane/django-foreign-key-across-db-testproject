0dc0d6a0ef7c4f404a293550871160ebc03a36da
Eliot <eliot@tapir>
2011-03-30 14:27:22 -0700

    Created brand new django project and 2 apps. Made no modifications

---
+++ foreign_key_across_db_testproject/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^foreign_key_accross_db_testproject/', include('foreign_key_accross_db_testproject.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-foreign-key-across-db-testproject",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Eliot",
+    author_email="saltycrane@gmail.com",
+    description="Django foreign key across db test project",
+    )
